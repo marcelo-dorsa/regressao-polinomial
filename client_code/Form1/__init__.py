f49f5349f85fbaf603c9f68e4bf125656bce1e55
marcelo.dorsaa@gmail.com <marcelo.dorsaa@gmail.com>
2022-09-18 14:32:57 +0000

    Edited settings and Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -21,14 +21,20 @@ class Form1(Form1Template):
     x = self.x_inputs.text
     y = self.y_inputs.text
     m = self.m_inputs.text
-    x_test = len(x.replace(" ", ""))
-    y_test = len(y.replace(" ", ""))
-    fig1 = anvil.server.call('plot', x, y, m)
+    x_test = [float(number.replace(" ", "")) for number in x.split(',')]
+    y_test = [float(number.replace(" ", "")) for number in y.split(',')]
+    if len(x_test) == len(y_test):
+      fig1 = anvil.server.call('plot', x, y, m)
+      self.plot_1.data = fig1
+    else:
+      alert(f"Vetor x e y possuem tamanhos diferentes.")
+      
+    #fig1 = anvil.server.call('plot', x, y, m)
     #self.image_1.source = fig1
-    self.plot_1.data = fig1
-    x_test = len(x.replace(" ", ""))
+    #self.plot_1.data = fig1
+    #x_test = len(x.replace(" ", ""))
     
-    y_test = len(y.replace(" ", ""))
+    #y_test = len(y.replace(" ", ""))
 
   
 
